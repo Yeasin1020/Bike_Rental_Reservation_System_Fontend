f42f8fc48766a781f5d533f38c1ee871fd24fe54
Yeasin1st <ysbd111@gmail.com>
2025-07-26 21:41:20 +0600

    login and signup background change

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -40,8 +40,8 @@ const SignUpForm: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen px-4 py-8 bg-gray-100 sm:px-6 lg:px-8">
-      <div className="w-full max-w-md p-8 space-y-4 bg-white rounded-lg shadow-md">
+    <div className="flex items-center justify-center min-h-screen px-4 py-8  sm:px-6 lg:px-8">
+      <div className="w-full max-w-md p-8 space-y-4  rounded-lg shadow-md">
         <h2 className="text-2xl font-bold text-center text-gray-800">
           Create a New Account
         </h2>
